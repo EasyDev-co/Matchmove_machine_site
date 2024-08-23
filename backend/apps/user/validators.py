9958b70b6be40dc972fa0081f48859457719559c
Александр Альхов <Aleksandr@MacBook-Air-2.local>
2024-08-23 13:48:39 +0300

    update validator

--- backend/apps/user/validators.py
+++ backend/apps/user/validators.py
@@ -4,16 +4,9 @@ from rest_framework import serializers
 
 def validate_custom_password(value):
     """"
-    Дополнительная валидация пароля.
+    Дополнительная валидация пароля,
+    остальные валидации уже есть в базовом модуле Джанго.
     """
-    if len(value) < 8:
-        raise serializers.ValidationError(
-            "Password must be at least 8 characters long."
-        )
-    if not re.search(r'[A-Za-z]', value):
-        raise serializers.ValidationError(
-            "Password must contain at least one letter."
-        )
     if not re.search(r'[0-9]', value):
         raise serializers.ValidationError(
             "Password must contain at least one digit."
