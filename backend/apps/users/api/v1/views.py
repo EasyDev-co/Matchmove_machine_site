09717fa3ab73366bad693bed4465020c689d1f34
Vlad <dr.melix@gmail.com>
2024-08-24 20:52:35 +0300

    MATCHMOVE-6: Added UserAPIView for retrieving and updating user information, ensuring authentication and permission checks

--- backend/apps/users/api/v1/views.py
+++ backend/apps/users/api/v1/views.py
@@ -1,25 +1,41 @@
+from django.db import IntegrityError
+from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from apps.users.models import User
 from .serializers import UserGetSerializer, UserUpdateSerializer
 
 
 class UserAPIView(APIView):
-    permission_classes = [
-        IsAuthenticated
-    ]
+    permission_classes = [IsAuthenticated]
 
     def get(self, request, *args, **kwargs):
         """Получение детальной информации о пользователе."""
-        user = request.user
-        serializer = UserGetSerializer(user)
-        return Response(serializer.data)
+        instance = request.user
+        user_data = User.objects.get(pk=instance.pk)
+        user_serializer = UserGetSerializer(user_data)
+        return Response(user_serializer.data)
 
     def patch(self, request, *args, **kwargs):
-        """Обновление информации о пользователе."""
-        user = request.user
-        serializer = UserUpdateSerializer(user, data=request.data, partial=True)
+        """Изменение информации о пользователе."""
+        instance = request.user
+        serializer = UserUpdateSerializer(
+            instance=instance, data=request.data, partial=True
+        )
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
+            try:
+                serializer.save()
+                return Response(data=serializer.data, status=status.HTTP_200_OK)
+            except IntegrityError as e:
+                error_message = str(e).lower()
+
+                if "email" in error_message:
+                    error_data = {"email": ["Адрес электронной почты недоступен"]}
+                elif "phone_number" in error_message:
+                    error_data = {"phone_number": ["Номер телефона недоступен"]}
+                else:
+                    error_data = {"message": "Произошла ошибка при сохранении"}
+
+                return Response(data=error_data, status=status.HTTP_400_BAD_REQUEST)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
