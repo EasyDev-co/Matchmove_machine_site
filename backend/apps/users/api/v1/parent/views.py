023c9704ff65281240bb47528bf1463adf907f3f
Vlad <dr.melix@gmail.com>
2024-08-25 18:15:47 +0300

    MATCHMOVE-7: Implemented PasswordChangeView to allow authenticated users to change their password securely

---
+++ backend/apps/users/api/v1/parent/views.py
@@ -0,0 +1,23 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+
+from .serializers import PasswordChangeSerializer
+
+
+class PasswordChangeView(APIView):
+    """Представление для смены пароля пользователя."""
+
+    permission_classes = [IsAuthenticated]
+
+    def patch(self, request, *args, **kwargs):
+        serializer = PasswordChangeSerializer(
+            data=request.data, context={"request": request}
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                {"message": "Пароль успешно изменен."}, status=status.HTTP_200_OK
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
