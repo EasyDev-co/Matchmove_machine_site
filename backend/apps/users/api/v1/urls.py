3a0ec17e53dbb3ad3b32eaa983f3df7e4b6c3f5c
alexalkhov <alex_0725@mail.ru>
2024-08-26 14:51:18 +0300

    feature reset passwort complited

--- backend/apps/users/api/v1/urls.py
+++ backend/apps/users/api/v1/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from apps.users.api.v1.views import (
-    UserRegisterView, UserLoginView, UserLogoutView, EmailVerificationCodeAPIView, ResetPasswordAPIView
+    UserRegisterView,
+    UserLoginView,
+    UserLogoutView,
+    EmailVerificationCodeAPIView,
+    ResetPasswordAPIView,
+    PasswordChangeAPIView,
 )
+
 
 urlpatterns = [
     path("register/", UserRegisterView.as_view(), name="register"),
@@ -9,4 +15,5 @@ urlpatterns = [
     path("logout/", UserLogoutView.as_view(), name="logout"),
     path("email_verification_code/", EmailVerificationCodeAPIView.as_view(), name="email_verification_code"),
     path("reset_password/", ResetPasswordAPIView.as_view(), name="reset_password"),
+    path("change_password/", PasswordChangeAPIView.as_view(), name="change_password"),
 ]
