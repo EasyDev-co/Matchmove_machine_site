f9196ac0aacf45f1c2206dec6ea09b2ef0daca85
Vlad <dr.melix@gmail.com>
2024-09-06 13:07:22 +0300

    MATCHMOVE-23: Add routing for Cart API and Order functionality

--- backend/apps/cart/api/v1/urls.py
+++ backend/apps/cart/api/v1/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from apps.cart.api.v1.views import CartAPIView
+from .views import CartAPIView
 
 urlpatterns = [
     path('cart/', CartAPIView.as_view(), name='cart'),
+    path('cart/<int:pk>/', CartAPIView.as_view(), name='cart_item'),
+    path('cart/checkout/', CartAPIView.as_view(), name='checkout'),
 ]
