da9368a285f5a7ee2d90d0fcc1525de5376e41a7
Vlad <dr.melix@gmail.com>
2024-09-11 13:33:21 +0300

    MATCHMOVE-24: Implement services for orders logic

---
+++ backend/apps/orders/services.py
@@ -0,0 +1,21 @@
+from apps.orders.models import Order
+from django.core.paginator import Paginator
+
+
+class OrderService:
+    def __init__(self, user):
+        self.user = user
+
+    def get_user_orders(self, page_number=1, items_per_page=15):
+        """Получение всех заказов пользователя с пагинацией."""
+        orders = Order.objects.filter(user=self.user).order_by("-created_at")
+        paginator = Paginator(orders, items_per_page)
+        return paginator.get_page(page_number)
+
+    def get_user_products(self, page_number=1, items_per_page=15):
+        """Получение всех продуктов пользователя с пагинацией."""
+        products = (
+            self.user.products.all()
+        )  # Предполагается, что у пользователя есть related_name 'products'
+        paginator = Paginator(products, items_per_page)
+        return paginator.get_page(page_number)
