4e228743883f2062cbbacf51ce1999ad0fbfd1d1
Vlad <dr.melix@gmail.com>
2024-09-11 13:38:04 +0300

    MATCHMOVE-24: Create API views for orders and products

--- backend/apps/orders/api/v1/views.py
+++ backend/apps/orders/api/v1/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from apps.orders.services import OrderService
+from .serializers import OrderSerializer
+from apps.products.api.v1.serializers import ProductSerializer
+from django.core.paginator import EmptyPage
 
-# Create your views here.
+
+class UserOrdersAPIView(APIView):
+    def get(self, request):
+        """Получение заказов пользователя с пагинацией."""
+        page_number = request.query_params.get("page", 1)
+        order_service = OrderService(user=request.user)
+        try:
+            orders = order_service.get_user_orders(page_number=page_number)
+            serializer = OrderSerializer(orders, many=True)
+            return Response(
+                {
+                    "orders": serializer.data,
+                    "total_pages": orders.paginator.num_pages,
+                    "current_page": orders.number,
+                }
+            )
+        except EmptyPage:
+            return Response(
+                {"error": "Page not found"}, status=status.HTTP_404_NOT_FOUND
+            )
+
+
+class UserProductsAPIView(APIView):
+    def get(self, request):
+        """Получение продуктов пользователя с пагинацией."""
+        page_number = request.query_params.get("page", 1)
+        order_service = OrderService(user=request.user)
+        try:
+            products = order_service.get_user_products(page_number=page_number)
+            serializer = ProductSerializer(products, many=True)
+            return Response(
+                {
+                    "products": serializer.data,
+                    "total_pages": products.paginator.num_pages,
+                    "current_page": products.number,
+                }
+            )
+        except EmptyPage:
+            return Response(
+                {"error": "Page not found"}, status=status.HTTP_404_NOT_FOUND
+            )
--- backend/apps/orders/services.py
+++ backend/apps/orders/services.py
@@ -14,8 +14,6 @@ class OrderService:
 
     def get_user_products(self, page_number=1, items_per_page=15):
         """Получение всех продуктов пользователя с пагинацией."""
-        products = (
-            self.user.products.all()
-        )  # Предполагается, что у пользователя есть related_name 'products'
+        products = self.user.products.all()
         paginator = Paginator(products, items_per_page)
         return paginator.get_page(page_number)
