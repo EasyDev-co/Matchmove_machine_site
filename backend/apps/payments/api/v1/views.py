497caf27db64fc7d8609abb90af072a8ebfa5542
Aleksandr Alkhov <alex_0725@mail.ru>
2024-09-13 22:56:29 +0300

    refactored code for transactions and added adress for consumer, now our transactions have status ready

--- backend/apps/payments/api/v1/views.py
+++ backend/apps/payments/api/v1/views.py
@@ -1,184 +1,65 @@
 from rest_framework import views
 from rest_framework.response import Response
-from django.conf import settings
-import requests
-
-from apps.products.models.products import Product
 from django.shortcuts import get_object_or_404
 import logging
+
+from apps.products.models.products import Product
+from apps.payments.services import (
+    fetch_or_create_customer,
+    fetch_or_create_product,
+    create_price,
+    create_transaction,
+    get_latest_address,
+    create_address
+)
+
 
 logger = logging.getLogger(__name__)
 
 
 class CreateTransactionAPIView(views.APIView):
+    """
+    Вьюсет для создания транзакции в Paddle.
+    Пример body api запроса:
+    {
+        "product_id": "e99955f1-cf57-4ac0-83f9-a5e4b89ad269",
+        "address": {
+            "description": "Head Office",
+            "first_line": "4050 Jefferson Plaza, 41st Floor",
+            "city": "New York",
+            "postal_code": "10021",
+            "region": "NY",
+            "country_code": "US"
+        }
+    }
+    Если адрес у клиента уже есть то его не нужно подтягивать в теле запроса
+    """
 
     def post(self, request):
-        # Получаем текущего пользователя
         user = request.user
-
-        # Пример данных для клиента
-        customer_data = {
-            "email": user.email,
-            "name": user.username,
-        }
-
-        # Ищем клиента по email через Paddle API
-        list_customers_url = 'https://sandbox-api.paddle.com/customers'
-        search_params = {'email': user.email}
-
-        customer_list_response = requests.get(
-            list_customers_url,
-            params=search_params,
-            headers={'Authorization': f'Bearer {settings.PADDLE_API_KEY}'}
-        )
-
-        # Проверяем ответ Paddle
-        if customer_list_response.status_code == 200:
-            customers_data = customer_list_response.json().get('data', [])
-            if customers_data:
-                # Если клиент найден, используем его ID
-                customer_id = customers_data[0]['id']
-                logger.info(f"Customer found with ID: {customer_id}")
-            else:
-                # Если клиента нет, создаем нового
-                logger.warning(f"Customer with email {user.email} not found. Creating new customer...")
-
-                customer_create_response = requests.post(
-                    'https://sandbox-api.paddle.com/customers',
-                    json=customer_data,
-                    headers={'Authorization': f'Bearer {settings.PADDLE_API_KEY}'}
-                )
-                
-                if customer_create_response.status_code != 200:
-                    logger.error(f"Error creating customer: {customer_create_response.text}")
-                    return Response(
-                        {"error": "Failed to create customer", "details": customer_create_response.text},
-                        status=customer_create_response.status_code
-                    )
-                
-                customer_id = customer_create_response.json()['data']['id']
-                logger.info(f"Successfully created new customer with ID: {customer_id}")
-        else:
-            logger.error(f"Error fetching customers: {customer_list_response.text}")
-            return Response(
-                {"error": "Failed to fetch customers", "details": customer_list_response.text},
-                status=customer_list_response.status_code
-            )
-
-
-        # Получаем продукт по ID из запроса
         product = get_object_or_404(Product, id=request.data['product_id'])
+        quantity = request.data.get('quantity', 1)
+        address_data = request.data.get('address', None)  # Получаем данные адреса из запроса
 
-        # Попытка найти продукт по имени (используем product.id)
-        search_product_url = 'https://sandbox-api.paddle.com/products'
-        search_params = {'name': str(product.id)}
+        try:
+            customer_id, _ = fetch_or_create_customer(user, address_data)
 
-        product_search_response = requests.get(
-            search_product_url,
-            params=search_params,
-            headers={'Authorization': f'Bearer {settings.PADDLE_API_KEY}'}
-        )
-
-        if product_search_response.status_code == 200:
-            product_data_list = product_search_response.json().get('data', [])
-            if product_data_list:
-                # Продукт найден, получаем его ID
-                product_id = product_data_list[0]['id']
-                logger.info(f"Product found with ID: {product_id}")
+            if address_data:
+                # Используем адрес из запроса
+                address_id = create_address(customer_id, address_data).get('id')
             else:
-                # Продукт не найден, создаем новый
-                product_data = {
-                    "name": str(product.id),
-                    "tax_category": "standard",
-                    "description": product.category or "",
-                    "type": "standard",
-                }
-                logger.info(f"Creating product with data: {product_data}")
-                product_create_response = requests.post(
-                    'https://sandbox-api.paddle.com/products',
-                    json=product_data,
-                    headers={'Authorization': f'Bearer {settings.PADDLE_API_KEY}'}
-                )
+                # Подтягиваем адрес из Paddle
+                address_id = get_latest_address(customer_id)
+                if not address_id:
+                    logger.warning(f"No existing address found for customer {customer_id}.")
 
-                if product_create_response.status_code != 201:
-                    return Response(
-                        {"error": "Failed to create product", "details": product_create_response.text},
-                        status=product_create_response.status_code
-                    )
-
-                product_id = product_create_response.json()['data']['id']
-                logger.info(f"Successfully created product with ID: {product_id}")
-        else:
-            return Response(
-                {"error": "Failed to search for products", "details": product_search_response.text},
-                status=product_search_response.status_code
+            product_id = fetch_or_create_product(product.id)
+            price_id = create_price(product_id, product.price)
+            transaction_response = create_transaction(
+                price_id, customer_id, quantity, address_id
             )
 
-
-        # Пример данных для цены
-        price_data = {
-            "description": "Base price for product",
-            "product_id": product_id,
-            "unit_price": {
-                #умножаем цену на 100 так как puddle принимает целые числа за центы
-                "amount": str(int(product.price * 100)),
-                "currency_code": "USD"
-            },
-            "name": "Standard Price",
-            "billing_cycle": None,
-            "trial_period": None,
-            "tax_mode": "account_setting",
-            "quantity": {
-                "minimum": 1,
-                "maximum": 100
-            }
-        }
-
-        # Логируем данные перед созданием цены
-        logger.info(f"Creating price with data: {price_data}")
-
-        # Создаем цену
-        price_response = requests.post(
-            'https://sandbox-api.paddle.com/prices',
-            json=price_data,
-            headers={'Authorization': f'Bearer {settings.PADDLE_API_KEY}'}
-        )
-
-        # Логируем статус ответа от Paddle
-        logger.info(f"Price creation response status: {price_response.status_code}")
-        logger.info(f"Price creation response data: {price_response.text}")
-
-        # Проверяем, если произошла ошибка
-        if price_response.status_code != 201:
-            logger.error(f"Error creating price: {price_response.text}")
-            return Response(
-                {"error": "Failed to create price", "details": price_response.text},
-                status=price_response.status_code
-            )
-
-        price_response_data = price_response.json()
-        price_id = price_response_data['data']['id']
-        logger.info(f"Successfully created price with ID: {price_id}")
-
-        # Формируем данные для транзакции
-        transaction_data = {
-            "items": [
-                {
-                    'price_id': price_id,
-                    "quantity": request.data.get('quantity', 1)
-                }
-            ],
-            "customer_id": customer_id,
-            "currency_code": "USD",
-            "collection_mode": "automatic"
-        }
-
-        # Проведение транзакции
-        transaction_response = requests.post(
-            'https://sandbox-api.paddle.com/transactions',
-            json=transaction_data,
-            headers={'Authorization': f'Bearer {settings.PADDLE_API_KEY}'}
-        )
-
-        # Возвращаем ответ
-        return Response(transaction_response.json())
+            return Response(transaction_response.json())
+        except Exception as e:
+            logger.error(f"Transaction error: {str(e)}")
+            return Response({"error": str(e)}, status=500)
---
+++ backend/apps/payments/services.py
@@ -0,0 +1,198 @@
+import requests
+import logging
+from django.conf import settings
+from apps.products.models.products import Product
+
+logger = logging.getLogger(__name__)
+
+
+def get_latest_address(customer_id):
+    """Получить последний адрес клиента."""
+    base_url = settings.PADDLE_API_BASE_URL
+    list_addresses_url = f'{base_url}/customers/{customer_id}/addresses'
+
+    try:
+        response = requests.get(
+            list_addresses_url,
+            headers={'Authorization': f'Bearer {settings.PADDLE_API_KEY}'}
+        )
+        response.raise_for_status()
+        addresses = response.json().get('data', [])
+
+        if addresses:
+            # Сортировка по дате обновления (последний адрес будет первым после сортировки)
+            addresses.sort(key=lambda x: x['updated_at'], reverse=True)
+            return addresses[0]['id']  # Возвращаем ID последнего адреса
+        else:
+            return None
+
+    except requests.RequestException as e:
+        logger.error(f"Error fetching addresses: {str(e)}")
+        raise Exception("Failed to fetch addresses")
+
+
+def create_address(customer_id, address_data):
+    """Создать новый адрес для клиента."""
+    base_url = settings.PADDLE_API_BASE_URL
+    create_address_url = f'{base_url}/customers/{customer_id}/addresses'
+
+    try:
+        response = requests.post(
+            create_address_url,
+            json=address_data,
+            headers={'Authorization': f'Bearer {settings.PADDLE_API_KEY}'}
+        )
+        response.raise_for_status()
+        return response.json().get('data', {})
+    except requests.RequestException as e:
+        logger.error(f"Error creating address: {str(e)}")
+        raise Exception("Failed to create address")
+
+
+def fetch_or_create_customer(user, address_data=None):
+    """Подтягиваем или создаем покупателя в Paddle."""
+    base_url = settings.PADDLE_API_BASE_URL
+    list_customers_url = f'{base_url}/customers'
+    search_params = {'email': user.email}
+
+    try:
+        response = requests.get(
+            list_customers_url,
+            params=search_params,
+            headers={'Authorization': f'Bearer {settings.PADDLE_API_KEY}'}
+        )
+        response.raise_for_status()
+        customers_data = response.json().get('data', [])
+        
+        if customers_data:
+            customer_id = customers_data[0]['id']
+            # Получаем последний адрес клиента
+            latest_address_id = get_latest_address(customer_id)
+            if address_data and not latest_address_id:
+                # Создаем новый адрес
+                created_address = create_address(customer_id, address_data)
+                latest_address_id = created_address.get('id')
+            return customer_id, latest_address_id
+        else:
+            # Создаем нового клиента
+            customer_data = {
+                "email": user.email,
+                "name": user.username,
+            }
+            logger.warning(f"Customer with email {user.email} not found. Creating new customer...")
+            create_response = requests.post(
+                f'{base_url}/customers',
+                json=customer_data,
+                headers={'Authorization': f'Bearer {settings.PADDLE_API_KEY}'}
+            )
+            create_response.raise_for_status()
+            customer_id = create_response.json()['data']['id']
+            if address_data:
+                created_address = create_address(customer_id, address_data)
+                address_id = created_address.get('id')
+            else:
+                address_id = None
+            return customer_id, address_id
+    except requests.RequestException as e:
+        logger.error(f"Error fetching or creating customer: {str(e)}")
+        raise Exception("Failed to fetch or create customer")
+
+
+def fetch_or_create_product(product_id):
+    """Подтягиваем или создаем продукт в Paddle."""
+    base_url = settings.PADDLE_API_BASE_URL
+    search_product_url = f'{base_url}/products'
+    search_params = {'name': str(product_id)}
+
+    try:
+        response = requests.get(
+            search_product_url,
+            params=search_params,
+            headers={'Authorization': f'Bearer {settings.PADDLE_API_KEY}'}
+        )
+        response.raise_for_status()
+        product_data_list = response.json().get('data', [])
+        
+        if product_data_list:
+            return product_data_list[0]['id']
+        else:
+            product = Product.objects.get(id=product_id)
+            product_data = {
+                "name": str(product_id),
+                "tax_category": "standard",
+                "description": product.category or "",
+                "type": "standard",
+            }
+            logger.info(f"Creating product with data: {product_data}")
+            create_response = requests.post(
+                f'{base_url}/products',
+                json=product_data,
+                headers={'Authorization': f'Bearer {settings.PADDLE_API_KEY}'}
+            )
+            create_response.raise_for_status()
+            return create_response.json()['data']['id']
+    except requests.RequestException as e:
+        logger.error(f"Error fetching or creating product: {str(e)}")
+        raise Exception("Failed to fetch or create product")
+
+
+def create_price(product_id, price_amount):
+    """Создаем цену продукта Paddle."""
+    base_url = settings.PADDLE_API_BASE_URL
+    price_data = {
+        "description": "Base price for product",
+        "product_id": product_id,
+        "unit_price": {
+            "amount": str(int(price_amount * 100)),  # Умножаем на 100 так как Paddle принимает целые числа за центы
+            "currency_code": "USD"
+        },
+        "name": "Standard Price",
+        "billing_cycle": None,
+        "trial_period": None,
+        "tax_mode": "account_setting",
+        "quantity": {
+            "minimum": 1,
+            "maximum": 100
+        }
+    }
+
+    try:
+        response = requests.post(
+            f'{base_url}/prices',
+            json=price_data,
+            headers={'Authorization': f'Bearer {settings.PADDLE_API_KEY}'}
+        )
+        response.raise_for_status()
+        return response.json()['data']['id']
+    except requests.RequestException as e:
+        logger.error(f"Error creating price: {str(e)}")
+        raise Exception("Failed to create price")
+
+
+def create_transaction(price_id, customer_id, quantity, address_id=None):
+    """Создаем транзакцию в Paddle."""
+    base_url = settings.PADDLE_API_BASE_URL
+    transaction_data = {
+        "items": [
+            {
+                'price_id': price_id,
+                "quantity": quantity
+            }
+        ],
+        "customer_id": customer_id,
+        "address_id": address_id,
+        "currency_code": "USD",
+        "collection_mode": "automatic",
+    }
+
+    try:
+        response = requests.post(
+            f'{base_url}/transactions',
+            json=transaction_data,
+            headers={'Authorization': f'Bearer {settings.PADDLE_API_KEY}'}
+        )
+        response.raise_for_status()
+        return response
+    except requests.RequestException as e:
+        logger.error(f"Failed to create transaction: {response.text}")
+        raise Exception("Failed to create transaction")
