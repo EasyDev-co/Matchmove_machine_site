4853fb23323b0b574ec52b3fed037e124d678639
Vlad <dr.melix@gmail.com>
2024-10-22 00:09:39 +0300

    oauth: google integrated

--- backend/apps/oauth/api/v1/urls.py
+++ backend/apps/oauth/api/v1/urls.py
@@ -1,20 +1,11 @@
 from django.urls import path
-# from apps.oauth.api.v1.providers import google, yandex, mailru
+from apps.oauth.api.v1.providers import google
 from apps.oauth.api.v1 import views
 
 urlpatterns = [
-    # # Google URLs
-    # path('login/google/', google.google_login, name='oauth_google_login'),
-    # path('callback/google/', google.google_callback, name='oauth_google_callback'),
-    #
-    # # Yandex URLs
-    # path('login/yandex/', yandex.yandex_login, name='oauth_yandex_login'),
-    # path('callback/yandex/', yandex.yandex_callback, name='oauth_yandex_callback'),
-    #
-    # # Mail.ru URLs
-    # path('login/mailru/', mailru.mailru_login, name='oauth_mailru_login'),
-    # path('callback/mailru/', mailru.mailru_callback, name='oauth_mailru_callback'),
-
+    # Google URLs
+    path('login/google/', google.google_login, name='oauth_google_login'),
+    path('callback/google/', google.google_callback, name='oauth_google_callback'),
     # Login
     path('login/oauth_token/', views.oauth_token_login, name='oauth_token_login'),
 
