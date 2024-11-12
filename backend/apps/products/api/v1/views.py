6266bbec091568e8354ae69958cf88594574f400
alexalkhov <alex_0725@mail.ru>
2024-11-12 15:36:57 +0300

    change permission for productsdetailview

--- backend/apps/products/api/v1/views.py
+++ backend/apps/products/api/v1/views.py
@@ -9,6 +9,7 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from rest_framework.pagination import PageNumberPagination
 from apps.utils.pagination import StandardPagination
+from apps.utils.permissions import IsAuthorOrReadOnly
 
 from apps.products.models import Camera, Format, Lens, Product, File
 from apps.products.tasks import (
@@ -70,10 +71,10 @@ class ProductCreateView(generics.CreateAPIView):
         return Response(response_data, status=status.HTTP_201_CREATED)
 
 
-class ProductDetailView(generics.RetrieveAPIView):
-    permission_classes = [AllowAny]
+class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthorOrReadOnly]
     queryset = Product.objects.all()
-    serializer_class = ProductDetailSerializer
+    serializer_class = ProductSerializer
 
 
 class CameraListView(ListAPIView):
