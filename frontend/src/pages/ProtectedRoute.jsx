d02b2402e1562d260473b842e38043e809d9ba9b
isaSyr <lisabenamisa@yahoo.com>
2024-11-22 16:33:41 +0300

    frontend-fixes-2:  fixed login popup bug

--- frontend/src/pages/ProtectedRoute.jsx
+++ frontend/src/pages/ProtectedRoute.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 const ProtectedRoute = ({ children }) => {
   const {isAuthenticated} = useSelector(state=> state.user)
+  const location = useLocation();
 
-  return isAuthenticated ? children : <Navigate to="/authorization" />;
+  return (isAuthenticated && location.pathname !== "/authorization") ? children : <Navigate to="/authorization" />;
 };
 
 export default ProtectedRoute;
