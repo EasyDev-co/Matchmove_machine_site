ce53ebf9455a8effe4468950133088ca58a82c7b
isaSyr <lisabenamisa@yahoo.com>
2024-12-12 16:15:07 +0300

    frontend-fixes-4: fixed the order of sidemenu buttons on FAQ, added display of current section; Added email redirect in FAQ and footer;

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,37 +1,84 @@
-
-import styles from "./Footer.module.css"
-import logo from "../../assets/images/logo.svg"
+import styles from "./Footer.module.css";
+import logo from "../../assets/images/logo.svg";
 import { useNavigate, useLocation } from "react-router-dom";
-import { visaIcon, masercardIcon, paypalIcon, americanExpressIcon, googlePayIcon, applePay } from "../../assets/svg/paymenticons";
-import { facebook, vimeo, instagram, linkedin, youtube } from "../../assets/svg/footerbtnhs";
+import {
+  visaIcon,
+  masercardIcon,
+  paypalIcon,
+  americanExpressIcon,
+  googlePayIcon,
+  applePay,
+} from "../../assets/svg/paymenticons";
+import {
+  facebook,
+  vimeo,
+  instagram,
+  linkedin,
+  youtube,
+} from "../../assets/svg/footerbtnhs";
 
 const Footer = () => {
-
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const location = useLocation();
 
-  const handleLogoPath = ()=>{
-    if (location.pathname !== '/') {
-      navigate('/');
+  const handleLogoPath = () => {
+    if (location.pathname !== "/") {
+      navigate("/");
     } else {
-      window.scrollTo({ top: 0, behavior: 'smooth' }); // Scroll to the top
+      window.scrollTo({ top: 0, behavior: "smooth" }); // Scroll to the top
     }
-  }
-    return (
-      <footer className={styles.container}>
-        <div className={styles.content}>
-          <div className={styles.logoCont}>
-            <img src={logo} alt="footer-logo" onClick={handleLogoPath} />
-          </div>
+  };
+  return (
+    <footer className={styles.container}>
+      <div className={styles.content}>
+        <div className={styles.logoCont}>
+          <img src={logo} alt="footer-logo" onClick={handleLogoPath} />
+        </div>
 
-          <div className={styles.menu}>
+        <div className={styles.menu}>
+          <div className={styles.column}>
+            <p className="h5-extra-bold">Menu</p>
+            <ul>
+              <li>
+                <a href="/library">Full library</a>
+              </li>
+              <li>
+                <a href="/how-it-works">How it works</a>
+              </li>
+              <li>
+                <a href="/affiliate-program">Affiliate program</a>
+              </li>
+              <li>
+                <a href="faq">FAQ</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className={styles.line}>
+          <div className={styles.lineCont} />
+        </div>
+
+        <div className={styles.wrap}>
+          <div className={styles.links}>
             <div className={styles.column}>
-              <p className="h5-extra-bold">Menu</p>
+              <p className="h5-extra-bold">Links</p>
               <ul>
-                <li><a href="/library">Full library</a></li>
-                <li><a href="/how-it-works">How it works</a></li>
-                <li><a href="/affiliate-program">Affiliate program</a></li>
-                <li><a href="faq">FAQ</a></li>
+                <li>
+                  <a href="/Privacy-Policy.pdf" target="_blank">
+                    Privacy policy
+                  </a>
+                </li>
+                <li>
+                  <a href="/Terms-of-Use.pdf" target="_blank">
+                    Terms of use
+                  </a>
+                </li>
+                <li>
+                  <a href="https://matchmovemachine.com/" target="_blank">
+                    Our studio
+                  </a>
+                </li>
               </ul>
             </div>
           </div>
@@ -40,87 +87,97 @@ const Footer = () => {
             <div className={styles.lineCont} />
           </div>
 
-          <div className={styles.wrap}>
-            <div className={styles.links}>
-              <div className={styles.column}>
-                <p className="h5-extra-bold">Links</p>
-                <ul>
-                  <li><a href="/Privacy-Policy.pdf"  target="_blank">Privacy policy</a></li>
-                  <li><a href="/Terms-of-Use.pdf" target="_blank">Terms of use</a></li>
-                  <li><a href="https://matchmovemachine.com/" target="_blank">Our studio</a></li>
-                </ul>
-              </div>
-            </div>
-
-            <div className={styles.line}>
-              <div className={styles.lineCont} />
-            </div>
-
-            <div className={styles.payments}>
-              <div className={styles.column}>
-                <p className="h5-extra-bold">Payments</p>
-                <ul>
-                  <li>
-                    <button>{visaIcon}</button>
-                  </li>
-                  <li>
-                    <button>{masercardIcon}</button>
-                  </li>
-                  <li>
-                    <button>{paypalIcon}</button>
-                  </li>
-                  <li>
-                    <button>{americanExpressIcon}</button>
-                  </li>
-                  <li>
-                    <button>{googlePayIcon}</button>
-                  </li>
-                  <li>
-                    <button>{applePay}</button>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-
-          <div className={styles.line}>
-            <div className={styles.lineCont} />
-          </div>
-
-          <div className={styles.item}>
-            <div className={styles.contacts}>
-              <div className={styles.column}>
-                <p className="h5-extra-bold">Contacts</p>
-                <ul>
-                  <li>
-                  <a href="https://www.facebook.com/matchmovemachine" target="_blank"><button>{facebook}</button> </a>
-                  </li>
-                  <li>
-                  <a href="https://vimeo.com/matchmovemachine" target="_blank"><button>{vimeo}</button></a>
-                  </li>
-                  <li>
-                  <a href="https://www.instagram.com/matchmovemachine" target="_blank"><button>{instagram}</button></a>
-                  </li>
-                  <li>
-                  <a href="https://www.linkedin.com/company/matchmovemachine/" target="_blank"><button>{linkedin}</button></a>
-                  </li>
-                  <li>
-                  <a href="https://www.youtube.com/@matchmovemachine" target="_blank"><button> {youtube}</button></a>
-                  </li>
-                </ul>
-              </div>
-
-              <div className={styles.email}>
-              <p className="h5-extra-bold">Email</p>
-              <p>grids@matchmovemachine.com</p>
-            </div>
+          <div className={styles.payments}>
+            <div className={styles.column}>
+              <p className="h5-extra-bold">Payments</p>
+              <ul>
+                <li>
+                  <button>{visaIcon}</button>
+                </li>
+                <li>
+                  <button>{masercardIcon}</button>
+                </li>
+                <li>
+                  <button>{paypalIcon}</button>
+                </li>
+                <li>
+                  <button>{americanExpressIcon}</button>
+                </li>
+                <li>
+                  <button>{googlePayIcon}</button>
+                </li>
+                <li>
+                  <button>{applePay}</button>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
-        <p className="h2-light">&copy; Matchmove machine, 2024</p>
 
-      </footer>
-    );
-  };
-  
-  export default Footer
+        <div className={styles.line}>
+          <div className={styles.lineCont} />
+        </div>
+
+        <div className={styles.item}>
+          <div className={styles.contacts}>
+            <div className={styles.column}>
+              <p className="h5-extra-bold">Contacts</p>
+              <ul>
+                <li>
+                  <a
+                    href="https://www.facebook.com/matchmovemachine"
+                    target="_blank"
+                  >
+                    <button>{facebook}</button>{" "}
+                  </a>
+                </li>
+                <li>
+                  <a href="https://vimeo.com/matchmovemachine" target="_blank">
+                    <button>{vimeo}</button>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.instagram.com/matchmovemachine"
+                    target="_blank"
+                  >
+                    <button>{instagram}</button>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.linkedin.com/company/matchmovemachine/"
+                    target="_blank"
+                  >
+                    <button>{linkedin}</button>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.youtube.com/@matchmovemachine"
+                    target="_blank"
+                  >
+                    <button> {youtube}</button>
+                  </a>
+                </li>
+              </ul>
+            </div>
+
+            <div className={styles.email}>
+              <p className="h5-extra-bold">Email</p>
+              <a
+                href="mailto:grids@matchmovemachine.com"
+                className={styles.highLight}
+              >
+                grids@matchmovemachine.com
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <p className="h2-light">&copy; Matchmove machine, 2024</p>
+    </footer>
+  );
+};
+
+export default Footer;
