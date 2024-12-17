80a5ddde47032b0c678d7230da32e42beb7df6d7
timofejkazakov <t.kazakov@agora.ru>
2024-12-17 23:40:31 +0300

    Правка пароля

--- backend/apps/users/validators.py
+++ backend/apps/users/validators.py
@@ -3,11 +3,20 @@ from rest_framework import serializers
 
 
 def validate_custom_password(value):
-    """"
-    Дополнительная валидация пароля.
-    """
-    if not re.match(r'^(?=.*[A-Za-z])(?=.*[0-9]).{8,}$', value):
+    """Дополнительная валидация пароля."""
+    if len(value) < 8:
         raise serializers.ValidationError(
-            "Password must be at least 8 characters long and contain both letters and digits."
+            "Password must be at least 8 characters long."
         )
+
+    if not re.search(r'[A-Za-z]', value):
+        raise serializers.ValidationError(
+            "Password must contain at least one letter."
+        )
+
+    if not re.search(r'\d', value):
+        raise serializers.ValidationError(
+            "Password must contain at least one digit."
+        )
+
     return value
