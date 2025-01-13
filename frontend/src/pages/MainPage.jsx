51c9a1958bda6cb4c5c1a7807e63b6ac8b7dcd0e
timofejkazakov <t.kazakov@agora.ru>
2025-01-13 22:16:38 +0300

    ДОбавил редирект

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import MainHeader from "../components/MainHeader/MainHeader";
 import MainAbout from "../components/MainAbout/MainAbout";
 import MainShare from "../components/MainShare/MainShare";
@@ -6,18 +7,30 @@ import TutorialPreview from "../components/TutorialPreview/TutorialPreview";
 import JoinCommunity from "../components/JoinCommunity/JoinCommunity";
 import ScrollToTopBtn from "../components/ScrollToTopBtn/ScrollToTopBtn";
 
-const MainPage = ()=>{
-    return (
-      <>
-      <MainHeader/>
-      <MainAbout/>
-      <MainShare/>
-      <HowItWorks/>
-      <TutorialPreview/>
-      <JoinCommunity/>
-      <ScrollToTopBtn/>
-      </>
-    );
-}
+const MainPage = () => {
+  const isRedirect = true;
 
-export default MainPage
+  useEffect(() => {
+    if (isRedirect) {
+      window.location.href = "https://grid.matchmovemachine.com/?_gl=1*v4u310*_gcl_au*MTk5MDQxNTM5Mi4xNzM2NzU5MzQ0";
+    }
+  }, [isRedirect]);
+
+  if (isRedirect) {
+    return null;
+  }
+
+  return (
+    <>
+      <MainHeader />
+      <MainAbout />
+      <MainShare />
+      <HowItWorks />
+      <TutorialPreview />
+      <JoinCommunity />
+      <ScrollToTopBtn />
+    </>
+  );
+};
+
+export default MainPage;
