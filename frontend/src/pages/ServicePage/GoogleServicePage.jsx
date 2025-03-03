67dce961ddab97732a394da91cd36669f51764c1
Kinyapin Yegor <egorkinyapin2004@mail.ru>
2025-03-03 21:22:14 +0300

    сервиспэйдж2

--- frontend/src/pages/ServicePage/GoogleServicePage.jsx
+++ frontend/src/pages/ServicePage/GoogleServicePage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import BASE_URL from '../../config';
+import Cookies from 'js-cookie';
 // import { useSearchParams } from 'react-router-dom';  // или другой способ парсинга query
 // import { API_GOOGLE_AUTH_ENDPOINT } from '../config';
 
