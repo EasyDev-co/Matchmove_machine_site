e39840b13a6894443bd8931e69f9753588b8d91b
Kinyapin Yegor <egorkinyapin2004@mail.ru>
2025-03-31 19:46:50 +0300

    vendor id

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -8,17 +8,17 @@ import JoinCommunity from "../components/JoinCommunity/JoinCommunity";
 import ScrollToTopBtn from "../components/ScrollToTopBtn/ScrollToTopBtn";
 
 const MainPage = () => {
-  const isRedirect = true;
+  // const isRedirect = true;
 
-  useEffect(() => {
-    if (isRedirect) {
-      window.location.href = "https://grid.matchmovemachine.com/?_gl=1*v4u310*_gcl_au*MTk5MDQxNTM5Mi4xNzM2NzU5MzQ0";
-    }
-  }, [isRedirect]);
+  // useEffect(() => {
+  //   if (isRedirect) {
+  //     window.location.href = "https://grid.matchmovemachine.com/?_gl=1*v4u310*_gcl_au*MTk5MDQxNTM5Mi4xNzM2NzU5MzQ0";
+  //   }
+  // }, [isRedirect]);
 
-  if (isRedirect) {
-    return null;
-  }
+  // if (isRedirect) {
+  //   return null;
+  // }
 
   return (
     <>
