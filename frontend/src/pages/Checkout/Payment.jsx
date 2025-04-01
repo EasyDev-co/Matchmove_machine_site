e9859c7dcdf87cce41746d8dbdb0dbfdf4dcdc32
Kinyapin Yegor <egorkinyapin2004@mail.ru>
2025-04-01 23:03:29 +0300

    initializq

--- frontend/src/pages/Checkout/Payment.jsx
+++ frontend/src/pages/Checkout/Payment.jsx
@@ -46,14 +46,14 @@ const Payment = ({ orderId }) => {
       script.onload = () => {
         // Скрипт загружен, можно (опционально) сразу инициализировать
         // window.Paddle?.Setup({ vendor: 189185 });
-        Paddle.Initialize({ vendor: 189185 });
+        Paddle.Initialize({ token: '5fb98e8e0d04f908df3af8ae742a53cb79927f7c2bd6d6e9a8' });
       };
       document.body.appendChild(script);
     } else {
       console.log('падл есть')
       // Если Paddle уже есть – инициализируем
       // window.Paddle?.Setup({ vendor: 189185 });
-      Paddle.Initialize({ vendor: 189185 });
+      Paddle.Initialize({ token: '5fb98e8e0d04f908df3af8ae742a53cb79927f7c2bd6d6e9a8' });
     }
   }, []);
 
@@ -124,7 +124,8 @@ const Payment = ({ orderId }) => {
       // Убедимся, что Paddle подгрузился
       if (window?.Paddle) {
         // Инициализируем (на всякий случай, если не сделали в useEffect)
-        window.Paddle.Setup({ vendor: 189185 });
+        // window.Paddle.Setup({ vendor: 189185 });
+        
         console.log('проверка 2')
         console.log('paymentId', paymentId)
         // Открываем поп-ап с оплатой через override
