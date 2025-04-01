0c4676e912ba89c59f51c82f49becd73fc6b73a7
Kinyapin Yegor <egorkinyapin2004@mail.ru>
2025-04-01 22:14:35 +0300

    initializq

--- frontend/src/pages/Checkout/Payment.jsx
+++ frontend/src/pages/Checkout/Payment.jsx
@@ -41,13 +41,14 @@ const Payment = ({ orderId }) => {
       script.async = true;
       script.onload = () => {
         // Скрипт загружен, можно (опционально) сразу инициализировать
-        window.Paddle?.Setup({ vendor: 189185 }); 
+        // window.Paddle?.Setup({ vendor: 189185 }); 
+        Paddle.Initialize({ token: '5fb98e8e0d04f908df3af8ae742a53cb79927f7c2bd6d6e9a8' });
       };
       document.body.appendChild(script);
     } else {
       // Если Paddle уже есть – инициализируем
-      window.Paddle.Setup({ vendor: 189185 });
-      // Paddle.Initialize({ token: '5fb98e8e0d04f908df3af8ae742a53cb79927f7c2bd6d6e9a8' });
+      // window.Paddle.Setup({ vendor: 189185 });
+      Paddle.Initialize({ token: '5fb98e8e0d04f908df3af8ae742a53cb79927f7c2bd6d6e9a8' });
     }
   }, []);
 
